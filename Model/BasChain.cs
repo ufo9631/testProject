914988e8b151174b002d6fc554dc6b5baa308c2c
4444 <44444444>
2016-06-23 15:20:48 +0800

    111111111111

---
+++ Model/BasChain.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    /// <summary>
+    /// 联锁表
+    /// </summary>
+   public class BasChain:BaseModel
+    {
+    }
+}
---
+++ Model/BasUsers.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    /// <summary>
+    /// 用户表
+    /// </summary>
+    public class BasUsers:BaseModel
+    {
+    }
+}
